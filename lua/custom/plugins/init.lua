bf8a9455da65d8cd596c33612cf586a724e4e1b5
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2024-12-02 19:58:45 +0700

    feat: Add Go LSP and Telescope file browser plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,42 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Go LSP
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+              gofumpt = true,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  -- File Browser
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      require("telescope").load_extension("file_browser")
+      vim.keymap.set(
+        "n",
+        "<leader>fb",
+        ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+        { noremap = true, desc = "Open File Browser" }
+      )
+    end,
+  },
+}

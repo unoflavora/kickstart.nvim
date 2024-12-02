7143e4cab3e5da6d6408e8a9fc68456e37bff038
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2024-12-02 19:54:18 +0700

    feat: Add custom extension plugin configuration template

---
+++ lua/custom/plugins/custom_extension.lua
@@ -0,0 +1,9 @@
+-- Custom extension plugin configuration
+return {
+  -- Plugin spec goes here
+  -- Example structure:
+  -- 'author/plugin-name',
+  -- config = function()
+  --   -- Plugin configuration
+  -- end,
+}
